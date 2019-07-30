34e2ab1ad3576b3fdc5eacfc225a44be0087f1a4
Jake Carlson <jakecarlson@Jakes-MBP.zyxel.com>
2019-07-30 11:40:51 -0500

    added variables to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,59 @@
 // Pseudo Code
+$(document).ready(function() {
 
 // 1) User loads page, START button appears to start game
+
+        //Variable with questions:
+    var questions = [{
+            //? 1
+        question: "Who is Gossip Girl?",
+        multChoice: ["Serena", "Blair", "Jenny", "Dan"],
+        answer: 3
+    }, {
+            //? 2
+        question: "Who came back from boarding school?",
+        multChoice: ["Vanessa", "Chuck", "Serena", "Nate"],
+        answer: 2
+    }, {
+            //? 3
+        question: "Where did Blair and Chuck have their first kiss?",
+        multChoice: ["Limo", "Eiffel Tower", "Empire State Building", "Empire Hotel"],
+        answer: 0
+    }, {
+            //? 4
+        question: "Who blackmails Serena?",
+        multChoice: ["Chuck", "Georgina", "Jenny", "Blair"],
+        answer: 1
+    }, {
+            //? 5
+        question: "What is made for Serena in the hotel kitchen?",
+        multChoice: ["Martini", "Cronut", "Salad", "Grilled Cheese"],
+        answer: 3
+    }]
+        // Answer Counter
+    var correctAnswer = 0;
+    var incorrectAnswer = 0;
+
+        // Current ? to save from the loop
+    var currentQuestion = 0;
+
+        // Selections by user to save
+    var unanswered = 0;
+    var answered = 0;
+    var userSelected = 0;
+
+        // Timers
+    var timer = 0;
+    var seconds = 0;
+
+        // Message screen for RESULTS SECTION
+    var results = {
+        correct: "Wow, you got it right!",
+        incorrect: "I can't believe you got it wrong...",
+        timesOver: "You took too long :( ",
+        gameOver: "Game is over!"
+    }
+    
 
 // 2) User clicks START> to get to QUESTION SECTION & displays following
 
@@ -39,4 +92,8 @@
     // d) Start Over Button
 
 // 6) If user clicks Start Over Button, Game starts over//// Page doesn't re-load!!!!!
+
+
+
+});
 
