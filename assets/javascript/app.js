cb7077c8acd3992b95ce64d18c1ed609ca7edc81
Jake Carlson <jakecarlson@Jakes-MBP.zyxel.com>
2019-07-30 09:53:27 -0500

    pseudo coded html & js, also got background image

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+// Pseudo Code
+
+// 1) User loads page, START button appears to start game
+
+// 2) User clicks START> to get to QUESTION SECTION & displays following
+
+    // a) Time Remaining Counter - counting down
+
+    // b) Question user has to answer
+
+    // c) Multiple choice answers to question above
+
+// 3) User clicks on an answer > Brings user to ANSWER SECTION & displays following
+
+    // a) Same Time Remaining Counter - counting down still
+
+    // b) "You're Correct/ Wrong !"
+
+    // c) "The correct answer was ______"
+
+    // d) **** Potential Gif...****************
+
+// 4) When Time Remaining Counter = 0, give new question & loop though until all ? answered
+
+// 5) When all questions are answered, load RESULTS SECTION & display following
+
+    // a) Time Remaining Counter - continues counting down
+
+    // b) "Games all done, here's how you did..."
+
+    // c) [Results Breakdown] -
+
+        // i) Correct Answers
+
+        // ii) Incorrect Answers 
+
+        // iii) Unanswered
+
+    // d) Start Over Button
+
+// 6) If user clicks Start Over Button, Game starts over//// Page doesn't re-load!!!!!
+
